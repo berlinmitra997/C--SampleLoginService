f5d31ea2d8b48875ee5956782543de6b80d5ce32
Berlin Mitra Putra <berlinmitra997@gmail.com>
2018-08-02 01:12:59 +0700

    First Commit

---
+++ FormLogin/FormLogin/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FormLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            logService.Service masuk = new logService.Service();
+            Boolean hasil;
+            hasil = masuk.loginService((textBox1.Text), (textBox2.Text));
+            if (hasil==true)
+            {
+                Form2 tampil = new Form2();
+                tampil.Show();
+                    this.Hide();
+            }
+            else 
+            {
+                label3.Text = "Gagal Masuk";
+            }
+        
+        }
+    }
+}
---
+++ LoginService/App_Code/Service.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public Boolean loginService(string username, string password)
+    {
+        string user, pass;
+        user = "berlin";
+        pass = "mitra";
+        if (username == user && password == pass)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+}
